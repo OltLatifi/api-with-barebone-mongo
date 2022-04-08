5d69dc4865527460ff84319f97fb51d42e1fa8ea
Olt Latifi <oltlatifi2003@gmail.com>
2022-04-08 12:10:38 +0200

    cors

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 const express = require("express");
+const cors = require("cors");
 const { connectToDb } = require("./db");
 
 const app = express();
+app.use(cors())
 app.use(express.json());
+
 // db connection
 connectToDb((error) => {
-  if (!error) {
-    // if everything is ok, the error doesnt exist,
-    // start listening
-    app.listen(3000, () => {
-      console.log("listening on port 3000");
-    });
-  }
+    if (!error) {
+        // if everything is ok, the error doesnt exist,
+        // start listening
+        app.listen(5000, () => {
+            console.log("listening on port 5000");
+        });
+    }
 });
 
 // routes
-const {getBooks, getOneBook, makeBook, deleteOneBook, updateOneBook} = require('./routes')
+const { getBooks, getOneBook, makeBook, deleteOneBook, updateOneBook } = require('./routes')
 
 // read
 app.get("/books", getBooks);
